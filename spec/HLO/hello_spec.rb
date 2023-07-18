672917123a778ac8429a48cb919afdea63d87949
sbx_user1051 <sbx_user1051@169.254.122.213>
2023-07-18 10:59:33 +0000

    Tue Jul 18 10:59:33 UTC 2023

--- spec/HLO/hello_spec.rb
+++ spec/HLO/hello_spec.rb
@@ -1,1 +1,12 @@
 require_relative '../../lib/solutions/HLO/hello'
+
+RSpec.describe Hello do
+  it "should return a greeting with the name" do
+    expect(Hello.new.hello("Tom")).to eq "Hello, Tom"
+  end
+
+  it "should return a greeting with a different name too" do
+    expect(Hello.new.hello("Mark")).to eq "Hello, Mark"
+  end
+end
+
