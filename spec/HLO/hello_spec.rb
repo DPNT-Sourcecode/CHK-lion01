52fd402dffdd3d23d1d1d9805557d557d5ce4dbe
sbx_user1051 <sbx_user1051@169.254.122.213>
2023-07-18 11:01:08 +0000

    Tue Jul 18 11:01:08 UTC 2023

--- spec/HLO/hello_spec.rb
+++ spec/HLO/hello_spec.rb
@@ -1,12 +1,11 @@
 require_relative '../../lib/solutions/HLO/hello'
 
 RSpec.describe Hello do
-  it "should return a greeting with the name" do
-    expect(Hello.new.hello("Tom")).to eq "Hello, Tom"
+  it "should return a string" do
+    expect(Hello.new.hello("Tom")).to eq "Hello, World!"
   end
 
-  it "should return a greeting with a different name too" do
-    expect(Hello.new.hello("Mark")).to eq "Hello, Mark"
+  it "should return a string regardless of name" do
+    expect(Hello.new.hello("Mark")).to eq "Hello, World!"
   end
 end
-
