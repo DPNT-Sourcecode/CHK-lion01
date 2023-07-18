0d567192665de472634562ed3aab31be95e725be
sbx_user1051 <sbx_user1051@169.254.122.213>
2023-07-18 10:52:13 +0000

    Tue Jul 18 10:52:13 UTC 2023

--- spec/SUM/sum_spec.rb
+++ spec/SUM/sum_spec.rb
@@ -1,1 +1,11 @@
 require_relative '../../lib/solutions/SUM/sum'
+
+RSpec.describe Sum do
+  it "should sum two numbers, 1 & 2" do
+    expect(Sum.new.sum(1, 2)).to eq 3
+  end
+
+  it "should sum two other numbers, 3 & 4" do
+    expect(Sum.new.sum(3, 4)).to eq 7
+  end
+end
