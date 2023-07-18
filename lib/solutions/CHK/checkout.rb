c2bd772d6846b87a62324fe29fa1412881c491c0
sbx_user1051 <sbx_user1051@169.254.122.213>
2023-07-18 11:32:35 +0000

    Tue Jul 18 11:32:35 UTC 2023

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -5,11 +5,16 @@ class Checkout
     count_A = skus.count("A") % 3
     special_A = skus.count("A") / 3
     count_B = skus.count("B") % 2
-    count_B = skus.count("B") / 2
+    special_B = skus.count("B") / 2
     count_C = skus.count("C")
     count_D = skus.count("D")
-    return -1 if skus.length > count_A + count_B + count_C + count_D
-    return count_A * 50 + count_B * 30 + count_C * 20 + count_D * 15
+
+    return -1 if skus.match(/[^ABCD]/)
+    
+    normal_offers = count_A * 50 + count_B * 30 + count_C * 20 + count_D * 15
+    special_offers = special_A * 130 + special_B * 45
+    return normal_offers + special_offers
   end
 
 end
+
